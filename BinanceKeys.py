af4c03ab8142ef6153eed4d13688c7321636f99b
Zhecho Manev <34834204+ZManev@users.noreply.github.com>
2024-11-17 16:28:02 +0200

    Update BinanceKeys.py

--- BinanceKeys.py
+++ BinanceKeys.py
@@ -1,3 +1,3 @@
-BinanceKey1 = {'api_key': 'YourApiKey',
-    'api_secret':'YourApiSecret'}
+BinanceKey1 = {'api_key': 'UiXWuiciQAiHY3bwzrXV6CMCNH8YvvOeuBIL1M669Bw1buhzQCtxhjzeQNFwkt3G',
+    'api_secret':'RZTlULazCVavYVNKXWS9NUiJm8mlzHUmYAaAcneTZ8A6ACuqwA6SVDuWGfjMAQk1U'}
 	
